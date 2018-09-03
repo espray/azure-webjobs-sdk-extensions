a852e6dcd2373f4f6becf85d3782b398413c06bc
Eric Spray <eric@connectbooster.com>
2018-09-03 12:29:31 -0500

    wip - tests

---
+++ WebJobs.Extensions.DependencyInjection.Tests/InjectAttributeTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace WebJobs.Extensions.DependencyInjection.Tests
+{
+    public class InjectAttributeTests
+    {
+        [Fact]
+        public void CreateInstance_works()
+        {
+            var attribute = new InjectAttribute();
+
+            Assert.IsType<InjectAttribute>(attribute);
+        }
+    }
+}
