5ca1728d7e1992de215362f823f629cdf7ac99aa
Eric Spray <eric@connectbooster.com>
2018-09-03 12:05:30 -0500

    Update documentation

--- src/WebJobs.Extensions.DependencyInjection/IServiceRegistration.cs
+++ src/WebJobs.Extensions.DependencyInjection/IServiceRegistration.cs
@@ -2,8 +2,15 @@
 
 namespace WebJobs.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Service registration interface
+    /// </summary>
     public interface IServiceRegistration
     {
+        /// <summary>
+        /// Configure services for the application. This may be called multiple times.
+        /// </summary>
+        /// <param name="services"></param>
         void ConfigureServices(IServiceCollection services);
     }
 }
